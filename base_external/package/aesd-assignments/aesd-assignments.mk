59bd68f5ac0b24ddf8d588d6b424f4da540920f2
kpakert <kpakert@gmail.com>
2023-10-15 10:02:51 -0500

    updating ass3 hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 72c0159f62e242605191c4eb5adb82ca0fce2cc4
+AESD_ASSIGNMENTS_VERSION = 819a59198d87fae5a49139421deecf2126db07d1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
