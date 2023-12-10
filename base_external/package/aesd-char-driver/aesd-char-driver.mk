7714752882e5d560651ea7bd9a44fab7da70c263
kpakert <kpakert@gmail.com>
2023-12-10 17:33:18 -0600

    updating to pull in new package aesd-char-driver

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_CHAR_DRIVER_VERSION = 0e0c6e1991577548e052087c5586b96e450c2aeb
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-kpakert.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver/
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
