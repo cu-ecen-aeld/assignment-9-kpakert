53f84ce8d426e3ff69070caa072a3fc475174d35
kpakert <kpakert@gmail.com>
2023-12-10 18:16:05 -0600

    update hash

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DRIVER_VERSION = d441845629b7af7d490505c01c9ee0e240f84213
+AESD_CHAR_DRIVER_VERSION = 3b4f8b3fd8ffb2c9e1df7cd6f1c1f2026be8233b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
